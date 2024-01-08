f6531ec033f134b72e3c14e7f8791cddd3a0120d
Georg <georgievw@yandex.ru>
2024-01-08 03:04:04 +0300

    New logic

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template, request
 from pymongo import MongoClient
 from bson import ObjectId
 
-DB_NAME = "library"
+DB_NAME = "new_library_test5"
 
 app = Flask(__name__)
 client = MongoClient()
 db = client[DB_NAME]
 persons = db["persons"]
 books = db["books"]
+loans = db["loans"]
 
 @app.route("/persons/")
 def get_persons():
@@ -31,9 +32,14 @@ def get_books():
 @app.route("/persons/<id>")
 def get_person(id):
     data = persons.find_one({"_id": ObjectId(id)})
+    data['open_loans'] = list(loans.find({"person_id": ObjectId(id), "fact_return_date": {"$exists": False}}, {"person_id": 0, "person_name": 0}))
+    data['closed_loans'] = list(loans.find({"person_id": ObjectId(id), "fact_return_date": {"$exists": True}}, {"person_id": 0, "person_name": 0}))
     return render_template('person.html', person=data) 
 
 @app.route("/books/<id>")
 def get_book(id):
     data = books.find_one({"_id": ObjectId(id)})
+    data['open_loan'] = loans.find_one({"book_id": ObjectId(id), "fact_return_date": {"$exists": False}}, {"book_id": 0, "book_title": 0})
+    data['closed_loans'] = list(loans.find({"book_id": ObjectId(id), "fact_return_date": {"$exists": True}}, {"book_id": 0, "book_title": 0}))
+    print(data)
     return render_template('book.html', book=data)
--- script.py
+++ script.py
@@ -4,16 +4,16 @@ import random
 from pymongo import MongoClient
 from faker import Faker
 
-DB_NAME = "library"
+DB_NAME = "new_library_test5"
 N_PERSONS = 10
 N_BOOKS = 20
-N_CLOSED_DEALS = 30
-N_OPEN_DEALS = 10
+N_LOANS = 30
 
 client = MongoClient()
 db = client[DB_NAME]
 persons = db["persons"]
 books = db["books"]
+loans = db["loans"]
 fake = Faker(['ru_RU'])
 
 for _ in range(N_PERSONS):
@@ -22,10 +22,8 @@ for _ in range(N_PERSONS):
     person['name'] = fake.name()
     person['address'] = fake.address()
     person['phone'] = fake.phone_number()
-    person['passport'] = fake.passport_number()
+    person['passport'] = fake.random_int(min=10**9, max=10**10-1)#fake.passport_number()
     person['registration_date'] = fake.date_time_between()
-    person['open_deals'] = []
-    person['closed_deals'] = []
 
     persons.insert_one(person)
 
@@ -33,52 +31,43 @@ for _ in range(N_BOOKS):
 
     book = dict()
     book['authors'] = [fake.name() for _ in range(fake.random_int(1, 2))]
-    book['title'] = f'Книга {fake.random_int(1, 100)}'
-    book['tags'] = [f'Тема {fake.random_int(1, 10)}' for _ in range(fake.random_int(1, 3))]
-    book['closed_deals'] = []
-
+    book['title'] = fake.catch_phrase()#f'Книга {fake.random_int(1, 100)}'
+    book['genre'] = [f'Жанр {fake.random_int(1, 10)}' for _ in range(fake.random_int(1, 1))]
+    book['isbn'] = fake.isbn13()
     books.insert_one(book)
 
-close_deal_persons = list(persons.find())
-close_deal_books = list(books.find())
-for _ in range(N_CLOSED_DEALS):
-    book = random.choice(close_deal_books)
-    person = random.choice(close_deal_persons)
+persons = list(persons.find({}, {'_id': 1, 'name': 1}))
+books = list(books.find({}, {'_id': 1, 'title': 1}))
+
+for _ in range(N_LOANS - N_BOOKS // 2):
     
-    deal = dict()
-    deal['issue_date'] = fake.date_time_between()
-    deal['return_date'] = fake.date_time_between()
-    deal['fact_return_date'] = fake.date_time_between()
+    loan = dict()
+    person = random.choice(persons)
+    loan['person_id'] = person['_id']
+    loan['person_name'] = person['name']
+    book = random.choice(books)
+    loan['book_id'] = book['_id']
+    loan['book_title'] = book['title']
+    loan['issue_date'] = fake.date_time_between()
+    loan['return_date'] = fake.date_time_between()
+    loan['fact_return_date'] = fake.date_time_between()
 
-    book_deal = deal.copy()
-    book_deal['person_id'] = person['_id']
-    book_deal['person_name'] = person['name']
+    loans.insert_one(loan)
 
-    person_deal = deal
-    person_deal['book_id'] = book['_id']
-    person_deal['book_title'] = book['title']
+books = iter(books)
+for _ in range(N_BOOKS // 2):
+    
+    loan = dict()
 
-    persons.update_one({'_id': person['_id']}, {'$addToSet': {'closed_deals': person_deal}})
-    books.update_one({'_id': book['_id']}, {'$addToSet': {'closed_deals': book_deal}})
+    person = random.choice(persons)
+    loan['person_id'] = person['_id']
+    loan['person_name'] = person['name']
 
-open_deal_persons = list(persons.find())
-for _ in range(min(N_OPEN_DEALS, N_BOOKS)):
-    person = random.choice(open_deal_persons)
-    book = books.find_one({'open_deal': None}) 
+    book = next(books)
+    loan['book_id'] = book['_id']
+    loan['book_title'] = book['title']
 
-    deal = dict()
-    deal['issue_date'] = fake.date_time_between()
-    deal['return_date'] = fake.date_time_between()
+    loan['issue_date'] = fake.date_time_between()
+    loan['return_date'] = fake.date_time_between()
 
-    book_deal = deal.copy()
-    book_deal['person_id'] = person['_id']
-    book_deal['person_name'] = person['name']
-
-    person_deal = deal
-    person_deal['book_id'] = book['_id']
-    person_deal['book_title'] = book['title']
-
-    persons.update_one({'_id': person['_id']}, {'$addToSet': {'open_deals': person_deal}})
-    books.update_one({'_id': book['_id']}, {'$set': {'open_deal': book_deal}})
-
-
+    loans.insert_one(loan)
